15416ade9f82e32a52bd77b7d02b71e3388fc5be
Thanh-Nam-1809 <vothanhnam18092001@gmail.com>
2024-03-21 17:00:55 +0700

    21/03/2024

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function stickyHeader() {
     header.classList.remove("sticky");
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var upButton = document.querySelector('.up');
+  
+  upButton.addEventListener('click', function() {
+      // Scroll to the top of the page
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth' // Optional smooth scrolling
+      });
+  });
+});
